0a36c356822c280b166ae13d227099d75f34e09b
Andrei Cozma <andreig992@gmail.com>
2020-06-04 00:56:11 -0500

    Added buttons

--- CalculatorSwiftUI/CalculatorSwiftUI/ContentView.swift
+++ CalculatorSwiftUI/CalculatorSwiftUI/ContentView.swift
@@ -10,7 +10,178 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack{
+            HStack{
+                Button(action: {
+                    print("C")
+                }) {
+                    Text("C").foregroundColor(Color.white)
+                }.frame(width: 80,height: 80)
+                .background(Color.blue)
+                .cornerRadius(50)
+                
+                Button(action: {
+                    print("+/-")
+                }) {
+                    Text("+/-").foregroundColor(Color.white)
+                }.frame(width: 80,height: 80)
+                .background(Color.blue)
+                .cornerRadius(50)
+                
+                Button(action: {
+                    print("%")
+                }) {
+                    Text("%").foregroundColor(Color.white)
+                }.frame(width: 80,height: 80)
+                .background(Color.blue)
+                .cornerRadius(50)
+                
+                Button(action: {
+                    print("/")
+                }) {
+                    Text("/").foregroundColor(Color.white)
+                }.frame(width: 80,height: 80)
+                .background(Color.blue)
+                .cornerRadius(50)
+            }
+            
+            HStack{
+                Button(action: {
+                    print("7")
+                }) {
+                    Text("7").foregroundColor(Color.white)
+                }.frame(width: 80,height: 80)
+                .background(Color.blue)
+                .cornerRadius(50)
+                
+                Button(action: {
+                    print("8")
+                }) {
+                    Text("8").foregroundColor(Color.white)
+                }.frame(width: 80,height: 80)
+                .background(Color.blue)
+                .cornerRadius(50)
+                
+                Button(action: {
+                    print("9")
+                }) {
+                    Text("9").foregroundColor(Color.white)
+                }.frame(width: 80,height: 80)
+                .background(Color.blue)
+                .cornerRadius(50)
+                
+                Button(action: {
+                    print("*")
+                }) {
+                    Text("*").foregroundColor(Color.white)
+                }.frame(width: 80,height: 80)
+                .background(Color.blue)
+                .cornerRadius(50)
+            }
+            
+            HStack{
+                Button(action: {
+                    print("4")
+                }) {
+                    Text("4").foregroundColor(Color.white)
+                }.frame(width: 80,height: 80)
+                .background(Color.blue)
+                .cornerRadius(50)
+                
+                Button(action: {
+                    print("5")
+                }) {
+                    Text("5").foregroundColor(Color.white)
+                }.frame(width: 80,height: 80)
+                .background(Color.blue)
+                .cornerRadius(50)
+                
+                Button(action: {
+                    print("6")
+                }) {
+                    Text("6").foregroundColor(Color.white)
+                }.frame(width: 80,height: 80)
+                .background(Color.blue)
+                .cornerRadius(50)
+                
+                Button(action: {
+                    print("-")
+                }) {
+                    Text("-").foregroundColor(Color.white)
+                }.frame(width: 80,height: 80)
+                .background(Color.blue)
+                .cornerRadius(50)
+            }
+            
+            HStack{
+                Button(action: {
+                    print("1")
+                }) {
+                    Text("1").foregroundColor(Color.white)
+                }.frame(width: 80,height: 80)
+                .background(Color.blue)
+                .cornerRadius(50)
+                
+                Button(action: {
+                    print("2")
+                }) {
+                    Text("2").foregroundColor(Color.white)
+                }.frame(width: 80,height: 80)
+                .background(Color.blue)
+                .cornerRadius(50)
+                
+                Button(action: {
+                    print("3")
+                }) {
+                    Text("3").foregroundColor(Color.white)
+                }.frame(width: 80,height: 80)
+                .background(Color.blue)
+                .cornerRadius(50)
+                
+                Button(action: {
+                    print("+")
+                }) {
+                    Text("+").foregroundColor(Color.white)
+                }.frame(width: 80,height: 80)
+                .background(Color.blue)
+                .cornerRadius(50)
+            }
+            
+            HStack{
+                Button(action: {
+                    print("0")
+                }) {
+                    Text("0").foregroundColor(Color.white)
+                }.frame(width: 80,height: 80)
+                .background(Color.blue)
+                .cornerRadius(50)
+                
+                Button(action: {
+                    print(".")
+                }) {
+                    Text(".").foregroundColor(Color.white)
+                }.frame(width: 80,height: 80)
+                .background(Color.blue)
+                .cornerRadius(50)
+                
+                Button(action: {
+                    print("?")
+                }) {
+                    Text("?").foregroundColor(Color.white)
+                }.frame(width: 80,height: 80)
+                .background(Color.blue)
+                .cornerRadius(50)
+                
+                Button(action: {
+                    print("=")
+                }) {
+                    Text("=").foregroundColor(Color.white)
+                }.frame(width: 80,height: 80)
+                .background(Color.blue)
+                .cornerRadius(50)
+                
+            }
+        }
     }
 }
 
