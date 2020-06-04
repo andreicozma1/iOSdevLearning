d258fcfa4a1156fff64f87dfc74114317288b9cb
Andrei Cozma <andreig992@gmail.com>
2020-06-03 20:47:26 -0500

    Added buttons project

--- Buttons/Buttons/ViewController.swift
+++ Buttons/Buttons/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var msgLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func defaultBtnClick(_ sender: Any) {
+        setText(text: "Default Btn")
+    }
+    
+    @IBAction func stateSelectedBtnClick(_ sender: Any) {
+        setText(text: "Selected Btn")
+    }
+    
+    @IBAction func stateDisabledBtnClick(_ sender: Any) {
+        setText(text: "Disabled Btn")
+    }
+    
+    @IBAction func stateHighlightedBtnClick(_ sender: Any) {
+        setText(text: "Highlighted Btn")
+    }
+    
+    @IBAction func pushBtnClick(_ sender: Any) {
+        setText(text: "PUSH BTN!!")
+    }
+    
+    func setText(text: String){
+        msgLabel.text = "Clicked \(text)"
+    }
 }
 
