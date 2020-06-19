8bc1aca5179c1c129512c2bf079576082a910ee6
Andrei Cozma <andreig992@gmail.com>
2020-06-19 02:18:10 -0500

    Working copy

--- CalculatorSwiftUI/CalculatorSwiftUI/ContentView.swift
+++ CalculatorSwiftUI/CalculatorSwiftUI/ContentView.swift
@@ -9,19 +9,24 @@
 import SwiftUI
 
 
-
 struct ContentView: View {
+    @State var display_t: String = "0"
+    
+    @State var left: Double? = 0
+    @State var right: Double? = nil
+    
     var body: some View {
+       
         VStack{
             
             Button(action:{
-                print("Clicked result view")
+                print("Clicked result view: " + self.display_t)
             }){
-                Text("0").font(Font.system(size: 40))
-                    .multilineTextAlignment(.trailing)
-                    .lineLimit(1)
-                    .foregroundColor(Color.white)
-                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 80, maxHeight: 80, alignment: .trailing)
+                Text(self.display_t).font(Font.system(size: 40))
+                .multilineTextAlignment(.trailing)
+                .lineLimit(1)
+                .foregroundColor(Color.white)
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 80, maxHeight: 80, alignment: .trailing)
                     .padding(EdgeInsets(top:0, leading:10, bottom:0,trailing:10))
             }
             .background(Color.blue)
@@ -81,17 +86,146 @@ struct ContentView: View {
             }.padding(3)
         }
     }
+    
+    @State var prev_state: String = ""
+    @State var cleared: Bool = false;
+    
+    func buildCalcButton(text: String) -> some View {
+        return Button(action: {
+            print("Clicked: \(text)")
+           
+            switch(text){
+                case "/", "*", "-","+", "%":
+                 
+                    if(self.cleared && self.left != nil){
+                        let num = Double(self.display_t)
+                        switch(self.prev_state){
+                        case "/":
+                            self.left! /= num!
+                            break
+                        case "*":
+                            self.left! *= num!
+                            break
+                        case "-":
+                            self.left! -= num!
+                            break
+                        case "+":
+                            self.left! += num!
+                            break
+                        case "%":
+                            // TODO - check if period
+                            if(!self.display_t.contains(".")){
+                                self.left = Double(Int(self.left!) % Int(num!))
+                            }
+                            break
+                        default:
+                            break
+                        }
+                        if(!self.display_t.contains(".") || self.prev_state != "%"){
+                            self.display_t = String(format:"%.8f", self.left!)
+                        }
+                        self.cleared = false
+                    } else{
+                        self.left = Double(self.display_t) ?? 0
+                        if(self.left == 0){
+                            self.cleared = true
+                        }
+                    }
+                    self.prev_state = text
+                
+                    break
+                case "=":
+                    print("\(self.cleared)")
+                    if(self.cleared && self.left != nil){
+                        print("\(self.left) and \(self.display_t) and \(self.prev_state)")
+                        let num = Double(self.display_t)
+                        switch(self.prev_state){
+                        case "/":
+                            self.left! /= num!
+                            break
+                        case "*":
+                            self.left! *= num!
+                            break
+                        case "-":
+                            self.left! -= num!
+                            break
+                        case "+":
+                            self.left! += num!
+                            break
+                        case "%":
+                            // TODO - check if period
+                            if(!self.display_t.contains(".")){
+                                self.left = Double(Int(self.left!) % Int(num!))
+                            }
+                            break
+                        default:
+                            break
+                        }
+                        if(!self.display_t.contains(".") || self.prev_state != "%"){
+                            self.display_t = String(format:"%.8f", self.left!)
+                        }
+                        self.prev_state = "="
+                        self.cleared = false
+                    }
+                break
+                case "C":
+                    self.prev_state = ""
+                    self.display_t = "0"
+                    self.left = 0
+                    self.right = nil
+                    self.cleared = false
+                    break
+                case "+/-":
+                    if(self.display_t.count > 0){
+                        if(self.display_t[self.display_t.startIndex] == "-"){
+                            self.display_t = String(self.display_t[self.display_t.index(self.display_t.startIndex, offsetBy: (1))...])
+                        } else{
+                            self.display_t = "-" + self.display_t
+                        }
+                    }
+                    
+
+                    break
+
+                case "?":
+                    break
+                case ".":
+                    if(!self.display_t.contains(".")){
+                        self.display_t += text
+                    }
+                        
+                    break
+                default:
+                    print("\(self.prev_state) and \(self.cleared)")
+                    if((self.prev_state != "" && !self.cleared) || self.display_t == "0"){
+                        if(self.display_t != "0"){
+                             self.cleared = true;
+                        }
+                        self.display_t = ""
+
+                    }
+                    if(self.prev_state == "="){
+                        self.cleared = false
+                        self.prev_state = ""
+                    }
+                    
+                    if(self.display_t.count < 13){
+                        self.display_t += text
+                    }
+            
+                    
+                    break
+            }
+
+        }) {
+            Text(text).foregroundColor(Color.white)
+                .frame(width: 80, height:80)
+        }
+        .buttonStyle(CalcButtonStyle())
+    }
 }
 
-func buildCalcButton(text: String) -> some View {
-    return Button(action: {
-        print(text)
-    }) {
-        Text(text).foregroundColor(Color.white)
-            .frame(width: 80, height:80)
-    }
-    .buttonStyle(CalcButtonStyle())
-}
+
 
 
 public struct CalcButtonStyle: ButtonStyle{
