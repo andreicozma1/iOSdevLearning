ff0d8895b0ad577cf6cf0db202a3808451d58c80
Andrei Cozma <andreig992@gmail.com>
2020-06-18 23:17:50 -0500

    Implemented button builder

--- CalculatorSwiftUI/CalculatorSwiftUI/ContentView.swift
+++ CalculatorSwiftUI/CalculatorSwiftUI/ContentView.swift
@@ -8,6 +8,8 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
     var body: some View {
         VStack{
@@ -27,199 +29,77 @@ struct ContentView: View {
             .padding()
             
             HStack{
-                Button(action: {
-                    print("C")
-                }) {
-                    Text("C")
-                        .foregroundColor(Color.white)
-                        .frame(width: 80, height:80)
-                }
-                .background(Color.blue)
-                .cornerRadius(50)
+                buildCalcButton(text: "C")
                 
-                Button(action: {
-                    print("+/-")
-                }) {
-                    Text("+/-").foregroundColor(Color.white)
-                        .frame(width: 80, height:80)
-                }
-                .background(Color.blue)
-                .cornerRadius(50)
+                buildCalcButton(text: "+/-")
                 
-                Button(action: {
-                    print("%")
-                }) {
-                    Text("%").foregroundColor(Color.white)
-                        .frame(width: 80, height:80)
-                }
-                .background(Color.blue)
-                .cornerRadius(50)
+                buildCalcButton(text: "%")
                 
-                Button(action: {
-                    print("/")
-                }) {
-                    Text("/").foregroundColor(Color.white)
-                        .frame(width: 80, height:80)
-                }
-                .background(Color.blue)
-                .cornerRadius(50)
+                buildCalcButton(text: "/")
             }.padding(3)
             
             HStack{
-                Button(action: {
-                    print("7")
-                }) {
-                    Text("7").foregroundColor(Color.white)
-                        .frame(width: 80, height:80)
-                }
-                .background(Color.blue)
-                .cornerRadius(50)
+                buildCalcButton(text: "7")
                 
-                Button(action: {
-                    print("8")
-                }) {
-                    Text("8").foregroundColor(Color.white)
-                        .frame(width: 80, height:80)
-                }
-                .background(Color.blue)
-                .cornerRadius(50)
+                buildCalcButton(text: "8")
                 
-                Button(action: {
-                    print("9")
-                }) {
-                    Text("9").foregroundColor(Color.white)
-                        .frame(width: 80, height:80)
-                }
-                .background(Color.blue)
-                .cornerRadius(50)
+                buildCalcButton(text: "9")
                 
-                Button(action: {
-                    print("*")
-                }) {
-                    Text("*").foregroundColor(Color.white)
-                        .frame(width: 80, height:80)
-                }
-                .background(Color.blue)
-                .cornerRadius(50)
+                buildCalcButton(text: "*")
+                
             }.padding(3)
             
             HStack{
-                Button(action: {
-                    print("4")
-                }) {
-                    Text("4").foregroundColor(Color.white)
-                        .frame(width: 80, height:80)
-                }
-                .background(Color.blue)
-                .cornerRadius(50)
+                buildCalcButton(text: "4")
                 
-                Button(action: {
-                    print("5")
-                }) {
-                    Text("5").foregroundColor(Color.white)
-                        .frame(width: 80, height:80)
-                }
-                .background(Color.blue)
-                .cornerRadius(50)
+                buildCalcButton(text: "5")
                 
-                Button(action: {
-                    print("6")
-                }) {
-                    Text("6").foregroundColor(Color.white)
-                        .frame(width: 80, height:80)
-                }
-                .background(Color.blue)
-                .cornerRadius(50)
+                buildCalcButton(text: "6")
                 
-                Button(action: {
-                    print("-")
-                }) {
-                    Text("-").foregroundColor(Color.white)
-                        .frame(width: 80, height:80)
-                }
-                .background(Color.blue)
-                .cornerRadius(50)
+                buildCalcButton(text: "-")
             }.padding(3)
             
             HStack{
-                Button(action: {
-                    print("1")
-                }) {
-                    Text("1").foregroundColor(Color.white)
-                        .frame(width: 80, height:80)
-                }
-                .background(Color.blue)
-                .cornerRadius(50)
+                buildCalcButton(text: "1")
                 
-                Button(action: {
-                    print("2")
-                }) {
-                    Text("2").foregroundColor(Color.white)
-                        .frame(width: 80, height:80)
-                }
-                .background(Color.blue)
-                .cornerRadius(50)
+                buildCalcButton(text: "2")
                 
-                Button(action: {
-                    print("3")
-                }) {
-                    Text("3").foregroundColor(Color.white)
-                        .frame(width: 80, height:80)
-                }
-                .background(Color.blue)
-                .cornerRadius(50)
+                buildCalcButton(text: "3")
                 
-                Button(action: {
-                    print("+")
-                }) {
-                    Text("+").foregroundColor(Color.white)
-                        .frame(width: 80, height:80)
-                }
-                .background(Color.blue)
-                .cornerRadius(50)
+                buildCalcButton(text: "+")
             }.padding(3)
             
             HStack{
-                Button(action: {
-                    print("0")
-                }) {
-                    Text("0").foregroundColor(Color.white)
-                        .frame(width: 80, height:80)
-                }
-                .background(Color.blue)
-                .cornerRadius(50)
+                buildCalcButton(text: "0")
                 
-                Button(action: {
-                    print(".")
-                }) {
-                    Text(".").foregroundColor(Color.white)
-                        .frame(width: 80, height:80)
-                }
-                .background(Color.blue)
-                .cornerRadius(50)
+               buildCalcButton(text: ".")
                 
-                Button(action: {
-                    print("?")
-                }) {
-                    Text("?").foregroundColor(Color.white)
-                        .frame(width: 80, height:80)
-                }
-                .background(Color.blue)
-                .cornerRadius(50)
+                buildCalcButton(text: "?")
                 
-                Button(action: {
-                    print("=")
-                }) {
-                    Text("=").foregroundColor(Color.white)
-                        .frame(width: 80, height:80)
-                }
-                .background(Color.blue)
-                .cornerRadius(50)
+                buildCalcButton(text: "=")
                 
             }.padding(3)
         }
     }
 }
+
+func buildCalcButton(text: String) -> some View {
+    return Button(action: {
+        print(text)
+    }) {
+        Text(text).foregroundColor(Color.white)
+            .frame(width: 80, height:80)
+    }
+    .buttonStyle(CalcButtonStyle())
+}
+
+
+public struct CalcButtonStyle: ButtonStyle{
+    public func makeBody(configuration: Configuration) -> some View {
+        configuration.label.background(Color.blue).cornerRadius(50)
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
